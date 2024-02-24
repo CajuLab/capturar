a7f361b72db59cb1574ccede32fa71a9434e4b1a
Luizmagao <luizmagao@gmail.com>
2024-02-24 11:22:52 -0300

    :ambulance: Processo ocm threshold

--- app.js
+++ app.js
@@ -28,21 +28,40 @@ const canvas = document.getElementById("canvas-output");
         // thresh
         let thresh = new cv.Mat();
         cv.threshold(gray, thresh, 90, 255, cv.THRESH_BINARY);
-        
 
-        // Desenha um retângulo na matriz
-        let startPoint = new cv.Point(10, 50);
-        let endPoint = new cv.Point(300, 200);
-        let color = new cv.Scalar(0, 255, 0); // (B, G, R)
-        let thickness = 5;
-        cv.rectangle(thresh, startPoint, endPoint, color, thickness);
+        // Encontrar contornos
+        let contours = new cv.MatVector();
+        let hierarchy = new cv.Mat();
+        cv.findContours(thresh, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
+
+        // Encontrar o maior contorno (assumindo que seja o retângulo)
+        let maxContour = null;
+        let maxArea = 0;
+        for (let i = 0; i < contours.size(); ++i) {
+            let contour = contours.get(i);
+            let area = cv.contourArea(contour);
+            if (area > maxArea) {
+                maxArea = area;
+                maxContour = contour;
+            }
+        }
+
+        // Desenhar o maior contorno (retângulo) na matriz
+        if (maxContour !== null) {
+            let rect = cv.boundingRect(maxContour);
+            let startPoint = new cv.Point(rect.x, rect.y);
+            let endPoint = new cv.Point(rect.x + rect.width, rect.y + rect.height);
+            let color = new cv.Scalar(255, 0, 0); // (B, G, R)
+            let thickness = 2;
+            cv.rectangle(thresh, startPoint, endPoint, color, thickness);
+        }
 
         // Adiciona texto à imagem
         let text = "Gabarito da prova";
         let fontFace = cv.FONT_HERSHEY_SIMPLEX;
-        let fontScale = 0.2;
+        let fontScale = 0.5;
         let textColor = new cv.Scalar(0, 255, 0); // (B, G, R)
-        let thicknessText = 1;
+        let thicknessText = 0.5;
         cv.putText(thresh, text, new cv.Point(10, 40), fontFace, fontScale, textColor, thicknessText, cv.LINE_AA);
 
         // Exibe a imagem no canvas
@@ -54,6 +73,8 @@ const canvas = document.getElementById("canvas-output");
         // Libera memória
         gray.delete();
         thresh.delete();
+        contours.delete();
+        hierarchy.delete();
     }
 
     setTimeout(processVideo, 0);
