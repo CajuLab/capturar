ad90af8bc60dab9fc0ff33e334ce3e49f825e10e
Luizmagao <luizmagao@gmail.com>
2024-02-24 19:23:58 -0300

    :art: Testando

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ const ctx = canvas.getContext("2d");  // Obtém o contexto 2D do canvas
         cv.putText(src, text, new cv.Point(10, 40), fontFace, fontScale, textColor, thicknessText, cv.LINE_AA);
         
         cv.rectangle(src, new cv.Point(20, 480), new cv.Point(100, 560), [255, 100, 0, 255], 2);
-        let regionOfInterest = src.roi(new cv.Rect(20,100,80,80))
+        let regionOfInterest = src.roi(new cv.Rect(20,480,80,80))
         
         const qrDetectCode = new cv.QRCodeDetector();
         let qrcode = new cv.Mat();
